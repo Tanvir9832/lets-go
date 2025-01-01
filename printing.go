63e074a1ca14700460d1bbebd9d535989da72d11
tanviAlam <tanvir9832@gmail.com>
2025-01-02 01:29:23 +0600

    printing

---
+++ printing.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func Printing() {
+	// Different Printing in go
+	fmt.Print("Hello, World!")
+
+	// Println
+	fmt.Println("Hello, World!")
+
+	// Printf
+	fmt.Printf("Hello, World!")
+
+	// Println with formatting
+	fmt.Println("Hello, World!", "This is a new line")
+
+	// Printf with formatting
+	fmt.Printf("Hello, World! %s", "This is a new line")
+
+	// Println with formatting
+	fmt.Println("Hello, World!", "This is a new line", "This is another new line")
+
+}
