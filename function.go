d9992b104f9baa35d2ae162dd51bd66b8efdb10b
tanviAlam <tanvir9832@gmail.com>
2025-01-05 23:59:52 +0600

    function

--- function.go
+++ function.go
@@ -6,10 +6,13 @@ func SimpleFunction() {
 	fmt.Println("Hello, World!")
 }
 
-
 //Function to add two numbers
 func Add(a, b int) int {
 	return a + b
 }
 
- 
+func myFunction(x int, y string) (result int, txt1 string) {
+	result = x + x
+	txt1 = y + " World!"
+	return
+}
