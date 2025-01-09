7d0824fda1dc993b8a5fe1c61b0cfda9821ca09d
tanviAlam <tanvir9832@gmail.com>
2025-01-09 14:56:03 +0600

    slice

--- slice/main.go
+++ slice/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+	slice := []int{}
+	fmt.Printf("Total slice is : %T\n", slice) // output: []int
+	fmt.Println("Total slice is : ", slice)    // output: []
+
+	sliceWithMake := make([]int, 5, 10)
+	fmt.Printf("Total slice is : %v", sliceWithMake) // output: [0 0 0 0 0]
+
+	sliceWithInitialValue := []int{1, 2, 3, 4, 5}
+	fmt.Printf("Total slice is : %v", sliceWithInitialValue) // output: [1 2 3 4 5]
+
+	//! Most of the thing coverd in array/main.go
 
 }
