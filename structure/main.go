823c98532ec7454af0ec2d3ee3ec09196e7368d3
tanviAlam <tanvir9832@gmail.com>
2025-01-10 22:42:06 +0600

    structure

---
+++ structure/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name       string
+	Age        int
+	University string
+	Department string
+	Cgpa       float64
+}
+
+func main() {
+	var tanvir Person
+	fmt.Println(tanvir) // { 0  0}
+	tanvir.Name = "Tanvir"
+	tanvir.Age = 22
+	tanvir.University = "Green University of Bangladesh"
+	tanvir.Department = "CSE"
+	tanvir.Cgpa = 1.50
+
+	fmt.Println(tanvir) // {Tanvir 22 Green University of Bangladesh CSE 1.5}
+
+	// Another way to declare struct
+	Anik := Person{
+		Name:       "Tanvir",
+		Age:        22,
+		University: "Green University of Bangladesh",
+		Department: "CSE",
+		Cgpa:       1.50,
+	}
+
+	fmt.Println(Anik) // {Tanvir 22 Green University of Bangladesh CSE 1.5}
+}
