81a87b52cf8e083d9aeeee1f41b2587c9e51fe86
tanviAlam <tanvir9832@gmail.com>
2025-01-12 13:10:46 +0600

    struct

--- structure/main.go
+++ structure/main.go
@@ -32,9 +32,42 @@ func main() {
 
 	fmt.Println(Anik) // {Tanvir 22 Green University of Bangladesh CSE 1.5}
 
-
-
-	type address struct {
-		
+	type Address struct {
+		Street  string
+		City    string
+		State   string
+		Zip     int
+		country string
 	}
+
+	type Contract struct {
+		email   string
+		phone   string
+		address Address
+	}
+
+	type Employee struct {
+		name     string
+		jobTitle string
+		contract Contract
+	}
+
+	firstEmployee := Employee{
+		name:     "Tanvir",
+		jobTitle: "Software Engineer",
+		contract: Contract{
+			email: "Tanvir@gmail.com",
+			phone: "+8801739783323",
+			address: Address{
+				Street:  "Dhanmondi",
+				City:    "Dhaka",
+				State:   "Dhaka",
+				Zip:     1205,
+				country: "Bangladesh",
+			},
+		},
+	}
+	firstEmployee.contract.address.City = "Gaza"
+	firstEmployee.contract.address.country = "Palestine"
+	fmt.Println(firstEmployee)
 }
