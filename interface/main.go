1f5373010a7e1a65f2d396b9e30f293558bdb4a9
Md. Tanvir Alam Anik <>
2025-06-07 21:11:12 +0600

    feat: add interface

--- interface/main.go
+++ interface/main.go
@@ -1,25 +1,27 @@
-
 package main
+
 import "fmt"
 
 // payment structure
-type payment struct {}
+type payment struct{}
 
 // method makePayment for payment
-func (p payment) makePayment (amount float64){
-	razorpayPaymentGw := razorpay {}
+func (p payment) makePayment(amount float64) {
+	razorpayPaymentGw := razorpay{}
 	razorpayPaymentGw.pay(amount)
 }
 
 // razorpay structure
-type razorpay struct {}
+type razorpay struct{}
 
 // method pay for razorpay
-func (r razorpay) pay (amount float64) {
+func (r razorpay) pay(amount float64) {
 	fmt.Println("making payment using razorpay", amount)
 }
 
-func main(){
+type stripe struct{}
+
+func main() {
 	newPayment := payment{}
 	newPayment.makePayment(100)
 }
