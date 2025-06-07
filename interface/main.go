743d613c0a8db906ffc24b2d862ecdd415301ab6
Md. Tanvir Alam Anik <>
2025-06-07 22:02:46 +0600

    feat: add interface

--- interface/main.go
+++ interface/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+//!1
 
 // payment structure
 type payment struct{}
@@ -19,9 +24,55 @@ func (r razorpay) pay(amount float64) {
 	fmt.Println("making payment using razorpay", amount)
 }
 
-type stripe struct{}
+// type stripe struct{}
+
+//! 1
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Circle struct {
+	Radius float32
+}
+
+type Rectangle struct {
+	Width, Height float64
+}
+
+func (c Circle) Area() float64 {
+	return float64(math.Pi) * float64(c.Radius) * float64(c.Radius)
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * float64(math.Pi) * float64(c.Radius)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func PrintShape(s Shape) {
+	fmt.Println(s.Area())
+	fmt.Println(s.Perimeter())
+}
 
 func main() {
 	newPayment := payment{}
 	newPayment.makePayment(100)
+
+	//! Shape
+	c := Circle{Radius: 5}
+	r := Rectangle{
+		Width:  5,
+		Height: 4,
+	}
+
+	PrintShape(c)
+	PrintShape(r)
 }
